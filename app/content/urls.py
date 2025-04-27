e715502420a74b59c761d36ec8d33665a20cdcb8
aziret <abdysovaziret307@gmail.com>
2025-04-28 01:13:05 +0600

    scrolling is ready

--- app/content/urls.py
+++ app/content/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, ReelViewSet
+from .views import PostViewSet, ReelViewSet, LoadMorePostsView, LoadMoreReelsView
 
+# Инициализируем роутер
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'reels', ReelViewSet)
 
 urlpatterns = [
+    # Подключаем стандартные ViewSet маршруты
     path('', include(router.urls)),
+
+    # Добавляем кастомные маршруты для пагинации
+    path('load-more-posts/', LoadMorePostsView.as_view(), name='load_more_posts'),
+    path('load-more-reels/', LoadMoreReelsView.as_view(), name='load_more_reels'),
 ]
--- app/content/views.py
+++ app/content/views.py
@@ -4,6 +4,60 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status, viewsets
+from rest_framework.views import APIView
+from rest_framework import status
+from django.core.paginator import Paginator
+
+class LoadMorePostsView(APIView):
+    def get(self, request):
+        page_number = int(request.GET.get('page', 1))
+        page_size = int(request.GET.get('size', 10))
+
+        all_items = Post.objects.all()  # Получаем все посты
+        paginator = Paginator(all_items, page_size)
+
+        try:
+            page = paginator.page(page_number)
+        except:
+            return Response({'error': 'Invalid page.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = PostSerializer(page.object_list, many=True)
+
+        response = {
+            'items': serializer.data,
+            'has_next': page.has_next(),
+            'has_previous': page.has_previous(),
+            'num_pages': paginator.num_pages,
+            'current_page': page.number,
+        }
+
+        return Response(response)
+
+
+class LoadMoreReelsView(APIView):
+    def get(self, request):
+        page_number = int(request.GET.get('page', 1))
+        page_size = int(request.GET.get('size', 10))
+
+        all_items = Reel.objects.all()  # Получаем все рилсы
+        paginator = Paginator(all_items, page_size)
+
+        try:
+            page = paginator.page(page_number)
+        except:
+            return Response({'error': 'Invalid page.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = ReelSerializer(page.object_list, many=True)
+
+        response = {
+            'items': serializer.data,
+            'has_next': page.has_next(),
+            'has_previous': page.has_previous(),
+            'num_pages': paginator.num_pages,
+            'current_page': page.number,
+        }
+
+        return Response(response)
 
 
 class PostViewSet(viewsets.ModelViewSet):
