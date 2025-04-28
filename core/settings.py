41ff4e5d5c87a848fa3022143acaad3f1f671590
aziret <abdysovaziret307@gmail.com>
2025-04-28 11:26:20 +0600

    server error

--- core/settings.py
+++ core/settings.py
@@ -1,16 +1,13 @@
-
-from datetime import timedelta
 from pathlib import Path
-
-
+from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-g56qefvrrgnx_ygpsz-#!ao71tv@!fjk49mukq68@lh#p*6%t+'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-g56qefvrrgnx_ygpsz-#!ao71tv@!fjk49mukq68@lh#p*6%t+')
 
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
 
 INSTALLED_APPS = [
     'jazzmin',
@@ -21,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     'drf_spectacular',
     'rest_framework',
     'rest_framework.authtoken',
@@ -29,7 +27,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
 
     'app.content',
-    'app.users'
+    'app.users',
 ]
 
 MIDDLEWARE = [
@@ -63,14 +61,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -97,20 +93,16 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
-
 LANGUAGE_CODE = 'ru-ru'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-
-
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
